346efd934c704a7c5a0585bdf57f549d5b03f431
Ronak Shah <shahronak47@yahoo.in>
2018-02-04 18:59:42 +0800

    get prices for shares from bse website

--- bsetools.py
+++ bsetools.py
@@ -9,20 +9,29 @@ from selenium import webdriver
 def get_bse_link(search_quote) :
     #Appending share price BSE to get a specific search result
     google_term = search_quote + " share price BSE"
-    search_results = google.search(search_quote)
+    search_results = google.search(google_term)
     for result in search_results:
         if "bseindia" in result.link:
             return result.link
 
 def get_price_from_bse(bse_link) :
     browser = webdriver.PhantomJS()
-    pdb.set_trace()
     browser.get(bse_link)
     html = browser.page_source
+    soup = BeautifulSoup(html)
+    #For values which are less than previous day are shown in red
+    value = soup.find('td', class_='tbmainred')
+    if value is None :
+        #For values which are less than previous day are shown in green
+        value = soup.find('td', class_='tbmaingreen')
+
+    return value.text.strip()
+
 
 def get_quote(company_name) :
     bse_link = get_bse_link(company_name)
     share_price = get_price_from_bse(bse_link)
+    print(share_price)
 
 if __name__ == '__main__' :
-    get_quote("Dolphin Offshore")
+    get_quote("Tech Mahindra")
