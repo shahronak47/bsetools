18e203a62ff4ad4825fde0eb708c8d5b9011a123
Ronak Shah <shahronak47@yahoo.in>
2018-02-04 23:48:28 +0800

    made the function as a package

--- bsetools.py
+++ bsetools.py
@@ -5,33 +5,38 @@ import requests
 import urllib
 from selenium import webdriver
 
+class bsetools() :
 
-def get_bse_link(search_quote) :
-    #Appending share price BSE to get a specific search result
-    google_term = search_quote + " share price BSE"
-    search_results = google.search(google_term)
-    for result in search_results:
-        if "bseindia" in result.link:
-            return result.link
+    def __init__(self):
+        #Constructor for this class.
+        self.board = "BSE"
 
-def get_price_from_bse(bse_link) :
-    browser = webdriver.PhantomJS()
-    browser.get(bse_link)
-    html = browser.page_source
-    soup = BeautifulSoup(html)
-    #For values which are less than previous day are shown in red
-    value = soup.find('td', class_='tbmainred')
-    if value is None :
-        #For values which are less than previous day are shown in green
-        value = soup.find('td', class_='tbmaingreen')
+    def get_bse_link(self, search_quote) :
+        #Appending share price BSE to get a specific search result
+        google_term = search_quote + " share price BSE"
+        search_results = google.search(google_term)
+        for result in search_results:
+            if "bseindia" in result.link:
+                return result.link
 
-    return value.text.strip()
+    def get_price_from_bse(self, bse_link) :
+        browser = webdriver.PhantomJS()
+        browser.get(bse_link)
+        html = browser.page_source
+        soup = BeautifulSoup(html)
+        #For values which are less than previous day are shown in red
+        value = soup.find('td', class_='tbmainred')
+        if value is None :
+            #For values which are less than previous day are shown in green
+            value = soup.find('td', class_='tbmaingreen')
+
+        return value.text.strip()
 
 
-def get_quote(company_name) :
-    bse_link = get_bse_link(company_name)
-    share_price = get_price_from_bse(bse_link)
-    print(share_price)
+    def get_quote(self, company_name) :
+        bse_link = self.get_bse_link(company_name)
+        share_price = self.get_price_from_bse(bse_link)
+        return share_price
 
-if __name__ == '__main__' :
-    get_quote("Tech Mahindra")
+#if __name__ == '__main__' :
+#    get_quote("Tech Mahindra")
