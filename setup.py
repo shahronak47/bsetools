719b3e39dff5cca3af016d88353e0a29fb4a08c4
Ronak Shah <shahronak47@yahoo.in>
2018-02-11 19:55:26 +0800

    redid the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'bsetools',
   packages = ['bsetools'],
-  version = '0.1.1',
+  version = '0.1',
   description = 'Get BSE data from bseindia.coms website',
   author = 'Ronak Shah',
   author_email = 'shahornak47@yahoo.in',
