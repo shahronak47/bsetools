94fc0482ab6e79cf7b30923473b1c2c6e8bebd39
Ronak Shah <shahronak47@yahoo.in>
2018-02-11 19:18:20 +0800

    added additional files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+  name = 'bsetools',
+  packages = ['bsetools'],
+  version = '0.1',
+  description = 'Get BSE data from bseindia.coms website',
+  author = 'Ronak Shah',
+  author_email = 'shahornak47@yahoo.in',
+  url = 'https://github.com/shahronak47/bsetools', 
+  download_url = 'https://github.com/shahronak47/bsetools/archive/0.1.tar.gz', keywords = ['testing', 'logging', 'example'], 
+  classifiers = [],
+)
