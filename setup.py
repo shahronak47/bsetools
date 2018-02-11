102db44075923e63cdc9037cf0a2c45cc38ee3fe
Ronak Shah <shahronak47@yahoo.in>
2018-02-11 22:07:50 +0800

    setup.py from Pycharm

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 
 setup(
-  name = 'bsetools',
-  packages = ['bsetools'],
-  version = '0.1',
-  description = 'Get BSE data from bseindia.coms website',
-  author = 'Ronak Shah',
-  author_email = 'shahornak47@yahoo.in',
-  url = 'https://github.com/shahronak47/bsetools', 
-  download_url = 'https://github.com/shahronak47/bsetools/archive/0.1.tar.gz', keywords = ['testing', 'logging', 'example'], 
-  classifiers = [],
+    name='bsetools',
+    version='0.1',
+    packages=['bsetools'],
+    url='https://github.com/shahronak47/bsetools',
+    license='MIT',
+    author='Ronak Shah',
+    author_email='shahronak47@yahoo.in',
+    description='Get BSE quotes'
 )
