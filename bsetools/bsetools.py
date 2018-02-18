982a05b84d4ee59afc304a17c54a22505e7488b8
Ronak Shah <shahronak47@yahoo.in>
2018-02-18 18:41:50 +0800

    get BSE index

--- bsetools/bsetools.py
+++ bsetools/bsetools.py
@@ -1,6 +1,8 @@
 from google import google
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import collections
+import pdb
 
 class bsetools() :
 
@@ -19,24 +21,31 @@ class bsetools() :
         #If there is a wrong search term or could not find bseindia link
         return "NA", False
 
+    def get_BSE_index(self, soup):
+        Sensex = collections.namedtuple('Sensex', ['bse_index', 'diff'])
+        Sensex.bse_index =  soup.find('div', class_='newsensexvalue').text.strip()
+        Sensex.diff = soup.find('div', class_='newsensextext2').text.strip()
+        return Sensex
+
     def get_price_from_bse(self, bse_link) :
         browser = webdriver.PhantomJS()
         browser.get(bse_link)
         html = browser.page_source
         soup = BeautifulSoup(html, "html.parser")
+        Sensex_data = self.get_BSE_index(soup)
         #For values which are less than previous day are shown in red
         value = soup.find('td', class_='tbmainred')
         if value is None :
             #For values which are greater than previous day are shown in green
             value = soup.find('td', class_='tbmaingreen')
 
-        return value.text.strip()
-
+        return value.text.strip(), Sensex_data
 
     def get_quote(self, company_name) :
         bse_link, flag = self.get_bse_link(company_name)
         if flag :
-            share_price = self.get_price_from_bse(bse_link)
-            return share_price
+            share_price, Sensex_data = self.get_price_from_bse(bse_link)
+            return share_price, Sensex_data
         else :
-           return "No relevant share price found for " + company_name
+            #Return relevant error message and empty bse index link in case there is no bse_link found
+           return "No relevant share price found for " + company_name, ""
