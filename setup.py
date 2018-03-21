ec761dcc150ddfd3fa90c90f302ac130d7602658
Ronak Shah <shahronak47@yahoo.in>
2018-03-21 21:32:06 +0800

    Changed the tag number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bsetools',
-    version='0.2.1',
+    version='0.2.2',
     packages=['bsetools'],
     url='https://github.com/shahronak47/bsetools',
     license='MIT',
