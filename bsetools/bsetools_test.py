d1a063efc98aa40cfacf70d111badb837d230f55
Ronak Shah <shahronak47@yahoo.in>
2018-09-30 16:47:02 +0800

    added unit tests for input and output types

--- bsetools/bsetools_test.py
+++ bsetools/bsetools_test.py
@@ -4,4 +4,14 @@ from bsetools import bsetools
 class TestBseTools(unittest.TestCase) :
 
     def test_types(self):
+        #Test the various input types
         self.assertRaises(TypeError, bsetools.get_quote, 23)
+        self.assertRaises(TypeError, bsetools.get_quote, True)
+
+    def test_isinstance(self):
+        #Test the output types
+        obj = bsetools()
+        quote, diff = obj.get_quote("Tech Mahindra")
+        self.assertIsInstance(quote, str)
+
+        self.assertIsInstance(diff, str)
